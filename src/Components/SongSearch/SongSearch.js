5b9f934a8f81157a673e338fb047648d467f3b76
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2022-10-31 14:00:36 +0000

    Add song guess placeholders

--- src/Components/SongSearch/SongSearch.js
+++ src/Components/SongSearch/SongSearch.js
@@ -4,7 +4,7 @@ import "./SongSearch.css";
 export function SongSearch({searchTerm, searchSuggestions, onChange, onFocus, inputNumber, show, mark, disabled}) {
   return (
     <div>
-      <input className={mark} autoComplete="off" id={inputNumber} type="text" list={show ? `song-selection-data-${inputNumber}` : null} value={searchTerm} onChange={onChange} onFocus={onFocus} disabled={disabled}></input>
+      <input className={mark} autoComplete="off" id={inputNumber} type="text" list={show ? `song-selection-data-${inputNumber}` : null} placeholder={`Guess ${inputNumber}`} value={searchTerm} onChange={onChange} onFocus={onFocus} disabled={disabled}></input>
       
       <datalist id={`song-selection-data-${inputNumber}`}>
         {searchSuggestions.map(suggestion => {
