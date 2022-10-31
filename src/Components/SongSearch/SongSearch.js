13758b9656126da5361596ce2b5f8d6d36e69342
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2022-10-31 17:04:23 +0000

    Add transition to show symbols when marking instead of changing input background colour

--- src/Components/SongSearch/SongSearch.js
+++ src/Components/SongSearch/SongSearch.js
@@ -1,14 +1,14 @@
 export function SongSearch({searchTerm, searchSuggestions, onChange, onFocus, inputNumber, show, mark, disabled}) {
   return (
     <div className="song-search">
-      <input className={mark} autoComplete="off" id={inputNumber} type="text" list={show ? `song-selection-data-${inputNumber}` : null} placeholder={`Guess ${inputNumber}`} value={searchTerm} onChange={onChange} onFocus={onFocus} disabled={disabled}></input>
-      <div className="tick-container">
+      <input autoComplete="off" id={inputNumber} type="text" list={show ? `song-selection-data-${inputNumber}` : null} placeholder={`Guess ${inputNumber}`} value={searchTerm} onChange={onChange} onFocus={onFocus} disabled={disabled}></input>
+      <div className={`tick-container${mark === "correct" ? " show" : ""}`}>
         <p>✓</p>
       </div>
-      <div className="squiggle-container">
+      <div className={`squiggle-container${mark === "wrong-place" ? " show" : ""}`}>
         <p>~</p>
       </div>
-      <div className="cross-container">
+      <div className={`cross-container${mark === "incorrect" ? " show" : ""}`}>
         <p>X</p>
       </div>
       <datalist id={`song-selection-data-${inputNumber}`}>
