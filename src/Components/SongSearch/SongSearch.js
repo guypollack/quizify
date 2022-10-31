be4241509081ee408315e47b7d0f23ef93b10444
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2022-10-31 15:48:56 +0000

    Create symbols next to answers to be shown/hidden on marking

--- src/Components/SongSearch/SongSearch.js
+++ src/Components/SongSearch/SongSearch.js
@@ -2,7 +2,15 @@ export function SongSearch({searchTerm, searchSuggestions, onChange, onFocus, in
   return (
     <div className="song-search">
       <input className={mark} autoComplete="off" id={inputNumber} type="text" list={show ? `song-selection-data-${inputNumber}` : null} placeholder={`Guess ${inputNumber}`} value={searchTerm} onChange={onChange} onFocus={onFocus} disabled={disabled}></input>
-      
+      <div className="tick-container">
+        <p>✓</p>
+      </div>
+      <div className="squiggle-container">
+        <p>~</p>
+      </div>
+      <div className="cross-container">
+        <p>X</p>
+      </div>
       <datalist id={`song-selection-data-${inputNumber}`}>
         {searchSuggestions.map(suggestion => {
           return <option key={suggestion} value={suggestion} />
