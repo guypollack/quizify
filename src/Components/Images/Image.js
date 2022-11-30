ee3a61e9384b2442a510b5b0e4eed112ad820b44
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2022-11-30 17:35:27 +0200

    Render each image immediately but make visible after timeout

--- src/Components/Images/Image.js
+++ src/Components/Images/Image.js
@@ -1,11 +1,10 @@
 export function Image({name, url, visibility, score}) {
-  if (visibility === "visible") {
-    return (
-      <div className="artist-image-container">
-        <img className="artist-image" src={url} />
-        <p>{name}</p>
-        <p>{score}/15</p>
-        {/* <p>{visibility}</p> */}
-      </div>)
-  }
+  const divClass = "artist-image-container " + visibility;
+  return (
+    <div className={divClass}>
+      <img className="artist-image" src={url} />
+      <p>{name}</p>
+      <p>{score}/15</p>
+      {/* <p>{visibility}</p> */}
+    </div>)
 }
